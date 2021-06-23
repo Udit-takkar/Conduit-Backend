7097514a43a84aaf8365a6e044ebedee0f46daa1
Udit Takkar <udit.07814802719@cse.mait.ac.in>
2021-06-23 19:48:39 +0530

    /api/profile unfollow endpoint added

--- routes/profiles.js
+++ routes/profiles.js
@@ -8,7 +8,7 @@ router.get("/:username", async (req, res) => {
   try {
     const { username } = req.params;
     const user = await User.findOne({ username });
-    res.status(200).send({ profile: user.toProfileJSONFor() });
+    return res.status(200).send({ profile: user.toProfileJSONFor() });
   } catch (err) {
     res.status(404).send({ err, message: "Not Found" });
   }
@@ -22,8 +22,28 @@ router.post("/:username/follow", verifyToken, async (req, res) => {
     const user = await User.findById({ _id: req.userData.sub });
     await user.follow(userToFollow._id);
 
-    res.status(200).send({ profile: userToFollow.toProfileJSONFor(user) });
-  } catch (err) {}
+    return res
+      .status(200)
+      .send({ profile: userToFollow.toProfileJSONFor(user) });
+  } catch (err) {
+    res.send(err);
+  }
+});
+// Unfollow a user
+router.delete("/:username/follow", verifyToken, async (req, res) => {
+  try {
+    const { username } = req.params;
+    const user = await User.findById({ _id: req.userData.sub });
+    const UserToUnfollow = await User.findOne({ username });
+
+    await user.unfollow(UserToUnfollow._id);
+
+    return res
+      .status(200)
+      .send({ profile: UserToUnfollow.toProfileJSONFor(user) });
+  } catch (err) {
+    res.send(err);
+  }
 });
 
 module.exports = router;
